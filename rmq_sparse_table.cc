dde5a92bc71a877a605fe37fa2505746d57c23e4
Ce Sun <sunce@ufl.edu>
2020-09-07 20:15:23 -0400

    add non-recursive disjoint-set

--- rmq_sparse_table.cc
+++ rmq_sparse_table.cc
@@ -2,20 +2,19 @@
 
 #include <algorithm>
 
+// preprocess O( n log(n) ), query O(1)
 struct rmq_sparse_table {
-#define K 25 // K should be floor(log2(n))
-    int (*st)[K + 1];
-    int *FLOOR_LOG2;
+#define K 25 // K should >= floor(log2(n))
+    int (*const st)[K + 1];
+    int *const FLOOR_LOG2;
 
-    rmq_sparse_table(const int *const arr, const int n) {
-        st = new int[n][K + 1];
+    rmq_sparse_table(const int *const arr, const int n)
+            : st(new int[n][K + 1]), FLOOR_LOG2(new int[n + 1]) {
         for (int i = 0; i < n; i++)
             st[i][0] = arr[i];
-
         for (int j = 1; j <= K; j++)
             for (int i = 0; i + (1 << j) <= n; i++)
                 st[i][j] = std::min(st[i][j - 1], st[i + (1 << (j - 1))][j - 1]);
-        FLOOR_LOG2 = new int[n + 1];
         FLOOR_LOG2[1] = 0;
         for (int i = 2; i <= n; i++)
             FLOOR_LOG2[i] = FLOOR_LOG2[i >> 1] + 1;
