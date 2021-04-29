b31af05655d6533a2dfd9d04aae79f2c6f1cd7d0
cesun <cesun@ufl.edu>
2021-04-29 23:26:10 +0800

    add lcs; fix max subarray sum

--- src/dp/lcs.cc
+++ src/dp/lcs.cc
@@ -1,2 +1,40 @@
-// cesun, 9/13/20 2:41 AM.
+// longest common subsequence
 
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int lcs_length_prototype(const string &a, const string &b) {
+    // dp[i][j]: length of lcs if only considering a[0,i-1] and b[0,j-1]
+    // We init all cell to 0 for now, but actually we only need to first row and first column to be 0
+    vector<vector<int>> dp(a.size() + 1, vector<int>(b.size() + 1, 0));
+    for (int i = 0; i < a.size(); i++) {
+        for (int j = 0; j < b.size(); j++) {
+            if (a[i] == b[j]) {
+                dp[i + 1][j + 1] = dp[i][j] + 1;
+            } else {
+                dp[i + 1][j + 1] = max(dp[i][j + 1], dp[i + 1][j]);
+            }
+        }
+    }
+    return dp[a.size()][b.size()];
+}
+
+constexpr int MAX_B_SZ = 1000;
+static int dp[2][MAX_B_SZ + 1];
+
+int lcs_length_optimized(const string &a, const string &b) {
+    memset(dp, 0, sizeof dp);
+    int curi, previ;
+    for (int i = 0; i < a.size(); i++) {
+        curi = i & 1, previ = curi ^ 1;
+        for (int j = 0; j < b.size(); j++) {
+            if (a[i] == b[j]) {
+                dp[curi][j + 1] = dp[previ][j] + 1;
+            } else {
+                dp[curi][j + 1] = max(dp[previ][j + 1], dp[curi][j]);
+            }
+        }
+    }
+    return dp[curi][b.size()];
+}
--- src/dp/max_subarray_sum.cc
+++ src/dp/max_subarray_sum.cc
@@ -1,13 +1,36 @@
-// cesun, 9/6/20 12:17 AM.
+#include <bits/stdc++.h>
 
-#include <algorithm>
-#include <climits>
+using namespace std;
 
-int_fast64_t max_subarray_sum(const int *const x, const int N) {
-    int_fast64_t dp = 0, max = INT_FAST64_MIN;
-    for (int i = 0; i < N; ++i) {
-        dp = std::max(dp + x[i], static_cast<int_fast64_t>(x[i]));
-        max = std::max(max, dp);
+// Given an integer array, find the max subarray sum.
+// This is trivial when all elements are positive, i.e. answer is summing up the whole array
+// Things get interesting when the array contains negative value, e.g. [5,4,-1,7,8].
+// We are willing to take the punishment caused by putting -1 into the sum,
+// because this allows us to make a longer subarray with larger sum.
+// But when the negative value is too large, e.g. [5,4,-90,7,8], we need to be smart enough to not include -90.
+
+int max_subarray_sum_prototype(vector<int> &a) {
+    vector<int> dp(a.size()); // dp[i]: the max sum of subarray that end exactly at a[i] (must include a[i])
+    dp[0] = a[0];
+    for (int i = 1; i < a.size(); i++) {
+        dp[i] = max(dp[i - 1] + x[i], x[i]);
     }
-    return max;
+    // and among subarrays with max sum that end exactly at each position, we find the max
+    int ans = INT_MIN;
+    for (int value : dp)
+        ans = max(ans, value);
+    return ans;
+}
+
+// We make 2 observations here:
+// 1. dp[i] only depends on dp[i-1] and x[i]
+// 2. Once dp[i] is computed, it never change.
+// These allows us to use only O(1) space, and do everything in 1 pass.
+int max_subarray_sum_optimized(vector<int> &a) {
+    int dp = 0, ans = INT_MIN; // mind arithmetic overflow though
+    for (int value : a) {
+        dp = max(dp + a, a);
+        ans = max(ans, dp);
+    }
+    return ans;
 }
